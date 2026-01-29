6698cb6583e37b95f40c3ae55a4107c72dcbc357
stydeveloper <stallyons.tester3@gmail.com>
2026-01-29 18:07:23 +0500

    Frontend Finalized

--- android/app/src/main/java/com/happystay/MainActivity.kt
+++ android/app/src/main/java/com/happystay/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.happystay
 
-import android.os.Bundle  // <-- ADD THIS LINE (you forgot this import!)
+import android.os.Bundle
+import android.graphics.Color
+import android.view.Window
+import android.view.WindowManager
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -12,6 +15,11 @@ class MainActivity : ReactActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(null)
+    
+    val window: Window = getWindow()
+    window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+    window.setStatusBarColor(Color.parseColor("#27AE60"))
   }
 
   override fun createReactActivityDelegate(): ReactActivityDelegate =
